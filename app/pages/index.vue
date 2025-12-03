cf141a37203c4ee85cecdf88f5aae14bb666bbab
arvidsson-geins <arvidsson@geins.io>
2025-12-03 13:22:23 +0100

    chore: wip

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-const { data: tenantConfig } = await useApi<TenantConfig>('/api/config');
-console.log('tenantConfig', tenantConfig.value);
 </script>
 <template>
   <div>{start page content}</div>
