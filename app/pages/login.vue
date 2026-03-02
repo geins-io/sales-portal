1a6785be600c393cc8b52ed47fa869239fa2eb5a
Ali Alhalaki <AliHalaki@outlook.com>
2026-03-02 21:41:14 +0300

    feat(SAL-18): wire forgot password flow in login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -7,6 +7,8 @@ definePageMeta({
 const route = useRoute();
 const router = useRouter();
 
+const authCardRef = ref<{ switchToForgot: () => void } | null>(null);
+
 const defaultView = computed(() =>
   route.query.tab === 'register' ? ('register' as const) : ('login' as const),
 );
@@ -19,15 +21,22 @@ function handleSuccess() {
 function handleClose() {
   router.replace('/');
 }
+
+function handleForgot() {
+  authCardRef.value?.switchToForgot();
+}
 </script>
 
 <template>
-  <AuthCard :default-view="defaultView" @close="handleClose">
+  <AuthCard ref="authCardRef" :default-view="defaultView" @close="handleClose">
     <template #login>
-      <AuthLoginForm @success="handleSuccess" />
+      <AuthLoginForm @success="handleSuccess" @forgot="handleForgot" />
     </template>
     <template #register>
       <AuthRegisterForm @success="handleSuccess" />
+    </template>
+    <template #forgot>
+      <AuthForgotPasswordForm />
     </template>
   </AuthCard>
 </template>
