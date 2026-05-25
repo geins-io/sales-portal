277b1ac916d92b9359ab58e08ca8885a700df480
Ali Alhalaki <AliHalaki@outlook.com>
2026-05-25 15:10:44 +0300

    fix(portal): flip apply page width nesting
    Bordered card was constrained to 670px while the page wrapper sat
    at the same width. Figma calls for the bordered container to span
    the standard 1280px page width and the form + cms area inside it
    to sit centered at 670px. Lift the outer wrapper to max-w-7xl and
    nest a max-w-[670px] container around the form and cms area.

--- app/pages/apply-for-account.vue
+++ app/pages/apply-for-account.vue
@@ -34,13 +34,15 @@ useHead({
 </script>
 
 <template>
-  <div class="mx-auto max-w-[670px] px-4 pt-8 pb-12 lg:px-6">
+  <div class="mx-auto max-w-7xl px-4 pt-8 pb-12 lg:px-6">
     <div class="border-border rounded-lg border p-6 md:p-8">
-      <CmsWidgetArea
-        v-if="applyCmsArea?.containers?.length"
-        :containers="applyCmsArea.containers"
-      />
-      <ApplyForAccountForm />
+      <div class="mx-auto max-w-[670px]">
+        <CmsWidgetArea
+          v-if="applyCmsArea?.containers?.length"
+          :containers="applyCmsArea.containers"
+        />
+        <ApplyForAccountForm />
+      </div>
     </div>
   </div>
 </template>
