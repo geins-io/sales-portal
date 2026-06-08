52f69463c636a7cd67ce7aaef337362d5e0e1edf
Ali Alhalaki <AliHalaki@outlook.com>
2026-06-08 15:17:15 +0300

    chore(apply): remove orphaned apply rate limiter

--- server/utils/rate-limiter.ts
+++ server/utils/rate-limiter.ts
@@ -209,12 +209,6 @@ export const newsletterRateLimiter = new RateLimiter({
   prefix: 'newsletter-subscribe',
 });
 
-export const applyForAccountRateLimiter = new RateLimiter({
-  limit: 3,
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  prefix: 'apply-account',
-});
-
 export const contactFormRateLimiter = new RateLimiter({
   limit: 3,
   windowMs: 15 * 60 * 1000, // 15 minutes
